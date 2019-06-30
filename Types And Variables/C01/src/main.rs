d83aefb859c19a1c6983b2cfe1ff5a7a7a7e7237
Jatinder Kumar Chaurasia <32101330+JatinderKumarChaurasia@users.noreply.github.com>
2019-06-30 20:15:14 +0530

    d
    Signed-off-by: Jatinder Kumar Chaurasia <32101330+JatinderKumarChaurasia@users.noreply.github.com>

--- Types And Variables/C01/src/main.rs
+++ Types And Variables/C01/src/main.rs
@@ -1,7 +1,7 @@
 fn main(){
     let a:u8 = 123; // 8 Bits Unsigned(u stands for unsigned 0 or positive and range is :0 to 255) //They are Immutable
     let b:i8 = 123; //8 Bits Signed (i  stands for signed integer and range is: -127 to 128 ) //They are Immutable
-    println!("The Value of Unsigned A is:{} and the Value of Signed B is:{}",a,b);
+    println!("The Value of Unsigned A is:{} and the Value of Scigned B is:{}",a,b);
     //Mutable
     let mut c:i8 = 0; //mut means mutable
 }
